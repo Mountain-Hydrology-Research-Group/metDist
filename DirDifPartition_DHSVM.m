5a452b8d85cea33cfe7f47fc639c4b614a577f98
Karl Lapo <lapo.karl@gmail.com>
2016-03-07 12:36:19 -0800

    First commit of spatial distribution repo. 4 scripts: DirDifPartition_DHSVM.m - Partitions direct-diffuse according to DHSVM's alg. MOQ_Hillshade.m - Hillshade algorithm from ArcGIS (as implemented in DHSVM) MOQ_Skyview.m - Skyview fraction from ArcGIS (as implemented in DHSVM) MOQ_Shading_OriginalTest.m - Old script that demonstrates use of above functions (see code block with YOS_DistributeSW_FLAG).
    MOQ is the project code for the Mountain QC project. These
    scripts/functions need to be updated, including a name change for this
    repo.

---
+++ DirDifPartition_DHSVM.m
@@ -0,0 +1,40 @@
+function [SDR_dir,SDR_dif] = DirDifPartition_DHSVM(EL,SWdwn)
+% This function will create a time series of direct-diffuse partitioning using
+% DHSVM's algorithm 
+%
+% SYNTAX:
+%	[SDR_dir,SDR_dif] = DirDifPartition_DHSVM(EL,SWdwn)
+%
+% INPUTS:
+%	EL	= Lx1 vector of solar elevation angles (angle above horizon)
+%
+% OUTPUTS
+%	Direct	= Lx1 vector of clear-sky direct component 
+%	Diffuse	= Lx1 vector of clear-sky diffuse component
+%
+
+
+%%%%%%%%%%%%
+%% Checks %%
+%%%%%%%%%%%%
+if numel(EL) ~= numel(SWdwn)
+	error('The number of elements in each entry must be the same length')
+end
+
+%%%%%%%%%%%%%%%%%
+%% "Constants" %%
+%%%%%%%%%%%%%%%%%
+
+S0 = sind(EL).*1367;						% Solar Constant (W/m^2) 
+CI = SWdwn./S0;								% "Clearness index"
+SDR_dif = NaN(size(SWdwn));
+SDR_dir = NaN(size(SWdwn));
+
+Fdif = .943 + .734.*CI - 4.9.*CI.^2 + 1.796.*CI.^3 + 2.058.*CI.^4;
+Fdif(CI > .8) = .13;
+Fdif(Fdif > 1) = 1;
+Fdif(Fdif < .13) = .13;
+Fdir = 1 - Fdif;
+
+SDR_dir = Fdir.*SWdwn;
+SDR_dif = Fdif.*SWdwn;
